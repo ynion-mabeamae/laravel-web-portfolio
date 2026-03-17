7078a1e1f47b53eaa3dd20cd69177213c70f48d8
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-03-17 23:55:27 +0800

    Remove tailwindcss plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
