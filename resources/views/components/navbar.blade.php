b1567634640fbba9b4b429d3ecb9172eeb94b4b5
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-03-18 00:02:36 +0800

    Add navbar component with responsive design and navigation links

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,72 @@
+<nav class="fixed top-0 w-full z-50 bg-gradient-to-r from-gray-900 via-black to-gray-900 border-b border-gray-700">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 py-3 sm:py-4">
+    <!-- Mobile and Tablet View (<= 1024px) -->
+    <div class="lg:hidden">
+      <div class="flex justify-center">
+        <ul class="flex flex-wrap justify-center gap-3 sm:gap-6">
+          <li>
+            <a href="#home" class="text-gray-300 hover:text-blue-500 text-sm sm:text-base transition-colors duration-300">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" class="text-gray-300 hover:text-blue-500 text-sm sm:text-base transition-colors duration-300">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" class="text-gray-300 hover:text-blue-500 text-sm sm:text-base transition-colors duration-300">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#projects" class="text-gray-300 hover:text-blue-500 text-sm sm:text-base transition-colors duration-300">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" class="text-gray-300 hover:text-blue-500 text-sm sm:text-base transition-colors duration-300">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Desktop View (> 1024px) -->
+    <div class="hidden lg:block">
+      <ul class="flex justify-center space-x-8 xl:space-x-12">
+        <li>
+          <a href="#home" class="text-gray-300 hover:text-blue-500 relative pb-1 inline-block group transition-colors duration-300">
+            Home
+            <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-500 transition-all duration-300 transform -translate-x-1/2 group-hover:w-full"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#about" class="text-gray-300 hover:text-blue-500 relative pb-1 inline-block group transition-colors duration-300">
+            About
+            <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-500 transition-all duration-300 transform -translate-x-1/2 group-hover:w-full"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#skills" class="text-gray-300 hover:text-blue-500 relative pb-1 inline-block group transition-colors duration-300">
+            Skills
+            <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-500 transition-all duration-300 transform -translate-x-1/2 group-hover:w-full"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#projects" class="text-gray-300 hover:text-blue-500 relative pb-1 inline-block group transition-colors duration-300">
+            Projects
+            <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-500 transition-all duration-300 transform -translate-x-1/2 group-hover:w-full"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#contact" class="text-gray-300 hover:text-blue-500 relative pb-1 inline-block group transition-colors duration-300">
+            Contact
+            <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-500 transition-all duration-300 transform -translate-x-1/2 group-hover:w-full"></span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
