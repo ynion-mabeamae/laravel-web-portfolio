cc505f22e470c25b118b73f57f2b66bcbe1afe39
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-03-29 17:34:38 +0800

    Upload template Laravel website

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
-Route::get('/', function () {
-    return view('main');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', [PortfolioController::class, 'index']);
+Route::post('/contact', [PortfolioController::class, 'storeMessage'])->name('contact.store');
