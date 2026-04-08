2253804a5e6cbaa7d3671882552652691401e34b
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-04-09 00:07:43 +0800

    fix: resolve pint style issues

--- app/Filament/Resources/Messages/Tables/MessagesTable.php
+++ app/Filament/Resources/Messages/Tables/MessagesTable.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\Messages\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
